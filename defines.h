ee3290292aea1002a833ba7e6a7a9f70c9606525
soptnrs <soptnrs@gmail.com>
2012-11-30 16:44:26 -0500

    Setting up the main module for the filesystem
    ramdisk_module.c file will contain the main definition of the file system
    defines.h will contain all of the macros used for accessing the filesystem
    
    I will also create another file for defining all of the IOCTL functions for
    the file system

---
+++ defines.h
@@ -0,0 +1,11 @@
+#include <linux/module.h>
+#include <linux/errno.h> /* error codes */
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <linux/tty.h>
+#include <linux/sched.h>
+#include <linux/interrupt.h>
+
+/*********************FILE SYSTEM FUNCTION INCLUDE**************************/
+
+/**************************IOCTL DECLARATIONS*******************************/
---
+++ ramdisk_ioctl.c
@@ -0,0 +1,76 @@
+/**************************************************************************/
+/**** Ramdisk IOCTL module for HW3 in Operating systems                ****/
+/**** The actual file system functions are stored in a separate file   ****/
+/**** This file contains initialization and the entry point            ****/
+/**************************************************************************/
+
+#include "defines.h"
+
+
+MODULE_LICENSE("GPL");
+
+/************************INIT AND EXIT ROUTINES*****************************/
+
+static int __init initialization_routine(void) {
+  printk("<1> Loading module\n");
+
+  pseudo_dev_proc_operations.ioctl = pseudo_device_ioctl;
+
+  /* Start create proc entry */
+  proc_entry = create_proc_entry("ioctl_test", 0444, NULL);
+  if(!proc_entry)
+  {
+    printk("<1> Error creating /proc entry.\n");
+    return 1;
+  }
+
+  //proc_entry->owner = THIS_MODULE; <-- This is now deprecated
+  proc_entry->proc_fops = &pseudo_dev_proc_operations;
+
+  return 0;
+}
+
+// Clean up routine
+static void __exit cleanup_routine(void) {
+
+  printk("<1> Dumping module\n");
+  remove_proc_entry("ioctl_test", NULL);
+
+  return;
+}
+
+/****************************IOCTL ENTRY POINT********************************/
+
+static int pseudo_device_ioctl(struct inode *inode, struct file *file,
+				unsigned int cmd, unsigned long arg)
+{
+  struct ioctl_test_t ioc;
+  struct keyboard_struct key;
+  
+  switch (cmd){
+
+  case IOCTL_TEST:
+    copy_from_user(&ioc, (struct ioctl_test_t *)arg, 
+		   sizeof(struct ioctl_test_t));
+    printk("<1> ioctl: call to IOCTL_TEST (%d,%c)!\n", 
+	   ioc.field1, ioc.field2);
+
+    my_printk ("Got msg in kernel\n");
+    break;
+
+  case KEYBOARD:
+    key.letter = my_getchar();
+    copy_to_user((struct keyboard_struct *)arg, &key, sizeof(struct keyboard_struct));
+    break;
+  
+  default:
+    return -EINVAL;
+    break;
+  }
+  
+  return 0;
+}
+
+// Init and Exit declaration
+module_init(initialization_routine); 
+module_exit(cleanup_routine); 
