ec3adb8eb9e988e96f90bb338a1320b0b2973671
soptnrs <soptnrs@gmail.com>
2012-12-05 15:55:04 -0500

    Even more fixes -_-

--- defines.h
+++ defines.h
@@ -1,11 +1,11 @@
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/vmalloc.h>
 #include <linux/errno.h> /* error codes */
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
 #include <linux/tty.h>
-#include <linux/sched.h>
-#include <linux/interrupt.h>
 
 /****************************IOCTL DECLARATIONS*******************************/
 
