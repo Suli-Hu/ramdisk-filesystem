e694811e3dd06064c2dbae2d105f2e5211c2701b
soptnrs <soptnrs@gmail.com>
2012-12-16 14:49:15 -0500

    Check existance

--- ramdisk_ioctl.c
+++ ramdisk_ioctl.c
@@ -607,7 +607,7 @@ int createIndexNode(char *type, char *pathname, int memorysize)
 
     /* Check if the file already exists */
     existance = getIndexNodeNumberFromPathname(pathname, 0);
-    PRINT("%s\n", pathname);
+    PRINT("%s %d\n", pathname, existance);
     if (existance != -1)
     {
         PRINT("File already exists\n");
